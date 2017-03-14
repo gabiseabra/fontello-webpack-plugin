18f042d69c1d85e9030d9924b7232e434a08c0c9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-03-14 06:28:46 -0300

    Added more tests

--- spec/FontelloPlugin.js
+++ spec/FontelloPlugin.js
@@ -1,16 +1,32 @@
-const _ = require("lodash")
+const Q = require("q")
 const should = require("should")
+const webpack = require("webpack")
+const MemoryFs = require("memory-fs")
 const FontelloPlugin = require("../src")
 const config = require("./config.json")
 
-/**
- * @todo - These tests
- */
+const webpackConfig = {
+	entry: "../package.json",
+	name: "icons",
+	output: {
+		path: "/",
+		filename: "[name].js"
+	}
+}
+
 describe("FontelloPlugin", () => {
-	let plugin = undefined;
-	
+	let fs, plugin
+
 	before(() => {
+		fs = new MemoryFs()
 		plugin = new FontelloPlugin({ config })
+		const compiler = webpack(Object.assign({}, webpackConfig, {
+			plugins: [ plugin ]
+		}))
+		compiler.outputFileSystem = fs
+		return Q.Promise((resolve, reject) => {
+			compiler.run((err, stats) => (err ? reject(err) : resolve(stats)))
+		})
 	})
 
 	describe("#assetUrl(type, ext?)", () => {
@@ -18,5 +34,14 @@ describe("FontelloPlugin", () => {
 			plugin.assetUrl("css").should.equal("icons.css")
 			plugin.assetUrl("font", "ttf").should.equal("font/icons.ttf")
 		})
+	})
+
+	it("emits font files", () => {
+		fs.existsSync("/font/icons.eot").should.be.ok()
+		fs.existsSync("/font/icons.ttf").should.be.ok()
+	})
+
+	it("emits a css file", () => {
+		fs.existsSync("/icons.css").should.be.ok()
 	})
 })
